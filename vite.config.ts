01296dd03e65d3c07d2f359fde822fa32ced1ecf
nicknick <nickback0@yandex.ru>
2025-10-28 19:14:02 +0300

    feat: tailwind config, main.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), viteTsconfigPaths()],
+  base: './',
+  plugins: [react(), viteTsconfigPaths(), tailwindcss()],
 });
